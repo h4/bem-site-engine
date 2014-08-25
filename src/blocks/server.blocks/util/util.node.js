0b8cf616f76b96c1466bc6c3af913479fc5f865b
Bemer <bemer@yandex-team.ru>
2014-08-25 14:34:39 +0400

    Add util method for retriece nodes by criteria

--- src/blocks/server.blocks/util/util.node.js
+++ src/blocks/server.blocks/util/util.node.js
@@ -1,5 +1,6 @@
 var path = require('path'),
 
+    _ = require('lodash'),
     vow = require('vow');
 
 modules.define('util', ['constants', 'config'], function(provide, constants, config) {
@@ -12,6 +13,50 @@ modules.define('util', ['constants', 'config'], function(provide, constants, con
          */
         isDev: function() {
             return 'development' === config.get('NODE_ENV');
+        },
+
+        /**
+         * Find node(s) which satisfy to criteria function
+         * @param model - {Object} sitemap model object
+         * @param criteria - {Function} criteria function
+         * @param onlyFirst - {Boolean} flag for find only first node
+         * @returns {*}
+         */
+        findNodesByCriteria: function(model, criteria, onlyFirst) {
+
+            var result = [];
+
+            if(!_.isObject(model)) {
+                return result;
+            }
+
+            if(!_.isFunction(criteria)) {
+                return result;
+            }
+
+            var isFound = function() {
+                    return onlyFirst && result.length;
+                },
+                traverseTreeNodes = function(node) {
+                    if(criteria.apply(node)) {
+                        result.push(node);
+                    }
+
+                    if(!isFound() && node.items) {
+                        node.items.forEach(function(item) {
+                            traverseTreeNodes(item);
+                        });
+                    }
+                };
+
+            model.forEach(function(node) {
+                if(isFound()) {
+                    return;
+                }
+                traverseTreeNodes(node);
+            });
+
+            return onlyFirst ? result[0] : result;
         }
     });
 });
